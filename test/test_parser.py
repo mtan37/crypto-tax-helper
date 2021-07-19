3e00a88605d52e2bb1ae5485e011adf60157b69c
Marvin Tan <marvin.tan@wisc.edu>
2021-07-19 03:00:41 +0000

    add unit test

---
+++ test/test_parser.py
@@ -0,0 +1,26 @@
+import unittest
+from renotaxhelper.helper import parser
+
+class TestTransactionData(unittest.TestCase):
+    
+    @classmethod
+    def setUpClass(self):
+        pass
+    
+    @classmethod
+    def tearDownClass(self):
+        pass
+    
+    def test_create_transactions_fail_empty(self):
+        with self.assertRaises(ValueError):
+            transactions = parser.Transactions()  
+        
+        with self.assertRaises(ValueError):
+            transactions = parser.Transactions("")  
+    
+    def test_create_transactions_fail_not_exist_symbol(self):
+        pass#TODO
+
+
+if __name__ == '__main__':
+    unittest.main()
