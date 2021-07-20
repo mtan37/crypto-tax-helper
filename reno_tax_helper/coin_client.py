9afba049ce4a8e9983166cbd608078efe3d0832f
Marvin Tan <marvin.tan@wisc.edu>
2021-07-20 23:13:28 +0000

    finish coin api call client

--- reno_tax_helper/coin_client.py
+++ reno_tax_helper/coin_client.py
@@ -1,20 +1,33 @@
 import requests
 import json
+import os
+import datetime
 
-COIN_API_KEY=os.env('COIN_API_KEY',"")
+COIN_API_KEY=os.getenv('COIN_API_KEY',"")
 COIN_BASE_URL='https://rest.coinapi.io'
-COIN_EXCAHNGE_RATE_URL_FORMAT='/v1/exchangerate/{crypto:}/{quote_currency:}?time={year:d}-{month:d}-{day:d}T00:00:00.0000000Z'
+COIN_EXCHANGE_RATE_URL_FORMAT='/v1/exchangerate/{crypto:}/{quote_currency:}?time={year:04d}-{month:02d}-{day:02d}T{hour:02d}:{minute:02d}:{second:02d}'
 
 def call(url, key):
     HEADERS = {
         'X-CoinAPI-Key': key,
     }
-    
-    try:
-        response = requests.get(url, headers=headers)
-        data = json.loads(response.text)
-        print(data)
+    response = requests.get(url, headers=HEADERS)
+    data = json.loads(response.text)
+    print(data)
+    return data
 
 def get_coin_value(crypto, quote_currency, date):
-    # TODO format the URL with parameters
-        
+    url = COIN_BASE_URL + \
+        COIN_EXCHANGE_RATE_URL_FORMAT.format(
+            crypto=crypto,
+            quote_currency=quote_currency,
+            year=date.year,
+            month=date.month,
+            day=date.day,
+            hour=date.hour,
+            minute=date.minute,
+            second=date.second,
+        )
+    return call(url,COIN_API_KEY)
+
+get_coin_value('RVN', 'USD', datetime.datetime(2021, 7, 19)) 
