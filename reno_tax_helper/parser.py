a37533ab855e424f9a6fe1ce1c26dff429e8a0a4
Marvin <marvin.tan@wisc.edu>
2022-03-18 15:55:17 -0500

    modify parser

--- reno_tax_helper/parser.py
+++ reno_tax_helper/parser.py
@@ -1,105 +1,60 @@
-import datetime
+from datetime import datetime
 from parse import *
-from reno_tax_helper.coin_client import get_coin_value, get_asset_info
 
 DATE_FORMAT_STR = "{year:04d}-{month:02d}-{day:02d}T{hour:02d}:{minute:02d}:{second:02d}{}"
 
-class Transactions:
+"""
+Only support RVN and USDT
+"""
+class Transaction:
     
-    def validate_currency(self, currency):
-        """
-            validate that the crypto currency is available in coinmarket cap
-        """
-        if not currency:
-           raise ValueError("Currency symbol can't be empty") 
-        
-        asset_info = get_asset_info(currency)
-        if not asset_info:
-           raise ValueError("Error getting asset info about currency ", currency) 
- 
-        return asset_info
-   
-    def validate_crypto_currency(self, crypto):
-        if not crypto:
-           raise ValueError("Crypto currency symbol can't be empty") 
-        
-        asset_info = self.validate_currency(crypto)
-        if not asset_info or not asset_info['type_is_crypto']:
-           raise ValueError("Crypto currency symbol is not a crypto") 
-        
-        return crypto
+    def __init__(self, date: datetime=None, amount: int=0):
+        self.date=self.validate_date(date)
+        # amount in crypto currency
+        self.amount=self.validate_amount(amount)
+        self.fiat_amount=None
 
-    def validate_quote_currency(self, quote):
-        if not quote:
-           raise ValueError("Quote currency symbol can't be empty") 
-        
-        asset_info = self.validate_currency(quote)
-        if not asset_info or asset_info['type_is_crypto']:
-           raise ValueError("Quote currency symbol is a crypto") 
-        
-        return quote
- 
-    def __init__(self, crypto_currency=None, quote_currency=None):
-        self.crypto_currency = self.validate_crypto_currency(crypto_currency)
-        self.quote_currency = self.validate_quote_currency(quote_currency)
-        self.ts_list=[]
-        self.ts_count=0
+    def validate_date(self, date):
+        if date is None:
+            raise ValueError("date value can't be None")
 
-    class Transaction:
-        
-        def validate_date(self, date):
-            if date is None:
-                raise ValueError("date value can't be None")
- 
-            if not isinstance(date, datetime.date):
-                # check if date has a valid format("2021-06-30T23:09:08")
-                date_parsed = parse(DATE_FORMAT_STR, date)
-                if date_parsed is None:
-                    raise ValueError("date parse error")
-                date = datetime.datetime(
-                    date_parsed.named['year'],
-                    date_parsed.named['month'],
-                    date_parsed.named['day'],
-                    hour=date_parsed.named['hour'],
-                    minute=date_parsed.named['minute'],
-                    second=date_parsed.named['second'],
-                )
+        if not isinstance(date, datetime):
+            # check if date has a valid format("2021-06-30T23:09:08")
+            date_parsed = parse(DATE_FORMAT_STR, date)
+            if date_parsed is None:
+                raise ValueError("date parse error")
+            date = datetime(
+                date_parsed.named['year'],
+                date_parsed.named['month'],
+                date_parsed.named['day'],
+                hour=date_parsed.named['hour'],
+                minute=date_parsed.named['minute'],
+                second=date_parsed.named['second'],
+            )
 
-            if date > datetime.datetime.now():
-                # check if is future date
-                raise ValueError("Can't have future dated transaction") 
-            
-            return date
+        if date > datetime.now():
+            # check if is future date
+            raise ValueError("Can't have future dated transaction") 
         
-        def validate_amount(self,amount):
-            amount = float(amount)
-            if amount <= 0:
-                raise ValueError("Transaction amount needs to be greater than 0")
-            return amount
-        
-        def __init__(self, date=None, amount=0):
-            self.date=self.validate_date(date)
-            self.amount=self.validate_amount(amount)
-            self.quote=None   
- 
-    def add_transaction(self, date, amount):
-        ts = self.Transaction(date,amount)
-        self.ts_list.append(ts)
-        self.ts_count+=1
-        value = get_coin_value(self.crypto_currency, self.quote_currency, ts.date)
-        if value is None:
-            raise Exception("Error getting coin value")
-        ts.quote = ts.amount * value
+        return date
+    
+    def validate_amount(self, amount):
+        amount = float(amount)
+        if amount <= 0:
+            raise ValueError("Transaction amount needs to be greater than 0")
+        return amount 
 
-def process_file(file_path, crypto_currency, quote_currency, start_index, date_index, amount_index):
+def process_file(file_path, start_row_index, date_index, amount_index):
     f = open(file_path, "r")
     #create a transaction
-    ts = Transactions(crypto_currency, quote_currency)
+    ts = []
     #add each transaction and calculate quote price for each transaction
     count = 0
+    start_date = None
+    end_date = None
     
     for l in f:
-        if count < start_index:
+        if count < start_row_index:
             count += 1
             continue
         
@@ -107,10 +62,15 @@ def process_file(file_path, crypto_currency, quote_currency, start_index, date_i
         date = list_l[date_index].strip('\"')
         amount = list_l[amount_index].strip('\"')
         try:
-            ts.add_transaction(date, amount)
+            transaction  = Transaction(date, amount)
+            ts.append(transaction)
+            if start_date is None or start_date > transaction.date:
+                start_date = transaction.date
+            if end_date is None or end_date < transaction.date:
+                end_date = transaction.date
         except ValueError as e:
             print(e)
             continue
   
     f.close()    
-    return ts
+    return ts, start_date, end_date
--- test/test_parser.py
+++ test/test_parser.py
@@ -1,117 +1,27 @@
-import unittest
-from unittest.mock import patch, call
-from reno_tax_helper.parser import Transactions , process_file
+from reno_tax_helper.parser import process_file
 
-class TestTransactionData(unittest.TestCase):
-    
-    @classmethod
-    def setUpClass(self):
-        self.is_crypto={'type_is_crypto': True}
-        self.not_crypto={'type_is_crypto': False}
-        self.date='2021-06-30T23:09:08'
-    @classmethod
-    def tearDownClass(self):
-        pass
-    
-    @patch('reno_tax_helper.parser.get_asset_info') 
-    def test_create_transactions_fail_no_arg(self, mocked_call):
-        with self.assertRaises(ValueError):
-            transactions = Transactions()
-        assert not mocked_call.called, 'get_asset_info should not be called'  
-    
-    @patch('reno_tax_helper.parser.get_asset_info') 
-    def test_create_transactions_fail_empty_arg(self, mocked_call):     
-        with self.assertRaises(ValueError):
-            transactions = Transactions("", "")  
-        assert not mocked_call.called, 'get_asset_info should not be called'  
-    
-    @patch('reno_tax_helper.parser.get_asset_info') 
-    def test_create_transactions_fail_not_exist_currency(self, mocked_call):
-        #invalid crypto, valid quote currency
-        mocked_call.side_effect = [None, self.not_crypto]
-        with self.assertRaises(ValueError):
-            transactions = Transactions("123", "USD")  
-        mocked_call.assert_called_with("123")
+# content of test_class_demo.py
+class TestParser:
+    def test_parse_file(self):
+        test_file_path='test/resource/test_parser.csv'
+        transactions, start_date, end_date = process_file(test_file_path, 1, 1, 5)
         
-    @patch('reno_tax_helper.parser.get_asset_info') 
-    def test_create_transactions_fail_not_exist_currency_quote(self, mocked_call):
-        #invalid crypto, invalid quote currency
-        mocked_call.side_effect = [None, None]
-        with self.assertRaises(ValueError):
-            transactions = Transactions("123", "123")
-        mocked_call.assert_called_with("123")
-        
-    @patch('reno_tax_helper.parser.get_asset_info') 
-    def test_create_transactions_fail_not_exist_quote(self, mocked_call):
-        #valid crypto, invalid quote currency
-        mocked_call.side_effect = [self.is_crypto, None]
-        with self.assertRaises(ValueError):
-            transactions = Transactions("RVN", "123")
-        mocked_call.assert_has_calls(
-            [call("RVN"), 
-            call("123")]
-        )
+        assert len(transactions) == 4
+        assert transactions[0].amount == 6
+        assert transactions[1].amount == 8
+        assert transactions[2].amount == 9
+        assert transactions[3].amount == 5
 
-    @patch('reno_tax_helper.parser.get_coin_value') 
-    @patch('reno_tax_helper.parser.get_asset_info') 
-    def test_add_transaction_successful(self, mocked_call, mocked_call2):
-        mocked_call.side_effect = [self.is_crypto, self.not_crypto]
-        mocked_call2.return_value = 1.0
-        transactions = Transactions("RVN","USD")
-        transactions.add_transaction(self.date,'20.0')
+        assert start_date.year == 2021
+        assert start_date.month == 6
+        assert start_date.day == 15
+        assert start_date.hour == 12
+        assert start_date.minute == 31
+        assert start_date.second == 45
 
-        assert transactions.ts_count == 1
-        assert transactions.ts_list[0].amount == 20
-        assert transactions.ts_list[0].quote == 20    
- 
-    @patch('reno_tax_helper.parser.get_coin_value') 
-    @patch('reno_tax_helper.parser.get_asset_info') 
-    def test_add_transaction_fail(self, mocked_call, mocked_call2):
-        mocked_call.side_effect = [self.is_crypto, self.not_crypto]
-        mocked_call2.return_value = 1.0
-        transactions = Transactions("RVN","USD")
-        
-        with self.assertRaises(ValueError):
-            transactions.add_transaction(None,'20.0')
-        
-        with self.assertRaises(ValueError):
-            transactions.add_transaction(self.date,0)
-
-class TestFileParsing(unittest.TestCase):
-    @classmethod
-    def setUpClass(self):
-        self.test_file_path='test/resource/test_parser.csv'
-        self.is_crypto={'type_is_crypto': True}
-        self.not_crypto={'type_is_crypto': False}
-        self.date='2021-06-30T23:09:08'
-    @classmethod
-    def tearDownClass(self):
-        pass
-    
-    @patch('reno_tax_helper.parser.get_coin_value') 
-    @patch('reno_tax_helper.parser.get_asset_info') 
-    def test_process_file_success(self, mocked_call, mocked_call2): 
-        mocked_call.side_effect = [
-            self.is_crypto, self.not_crypto,
-            self.is_crypto, self.not_crypto,
-            self.is_crypto, self.not_crypto,
-            self.is_crypto, self.not_crypto,
-            ]
-        mocked_call2.return_value = 2.0
-        transactions = process_file(self.test_file_path, 'RVN', 'USD', 1, 1, 5)
-        
-        assert transactions.ts_count == 4
-        assert int(transactions.ts_list[0].amount) == 6
-        assert transactions.ts_list[0].quote == 12    
-             
-        assert transactions.ts_list[1].amount == 8
-        assert transactions.ts_list[1].quote == 16    
-        
-        assert transactions.ts_list[2].amount == 9
-        assert transactions.ts_list[2].quote == 18    
-    
-        assert transactions.ts_list[3].amount == 5
-        assert transactions.ts_list[3].quote == 10
-    
-if __name__ == '__main__':
-    unittest.main()
+        assert end_date.year == 2021
+        assert end_date.month == 6
+        assert end_date.day == 30
+        assert end_date.hour == 23
+        assert end_date.minute == 9
+        assert end_date.second == 8
